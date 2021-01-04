9e4ce62a21732ae81ac301e08f27aef18b306659
moparisthebest <admin@moparisthebest.com>
2021-01-03 20:29:09 -0500

    Add Dreamcast pinout

---
+++ src/Dreamcast.cpp
@@ -0,0 +1,26 @@
+/*
+      LOOKING AT THE PLUG ON FRONT OF CONSOLE (not coming from controller)
+
+               6
+     ---------------------
+     |         o         |
+     |                   |
+     |     1   3   5     |
+     |     o   o   o     |
+     |                   |
+      \      o   o      /
+       \     2   4     /
+        `-.         .-'
+            ~-._.-~
+
+   PIN # USAGE (colors from my extension cable, check your own)
+
+  1:  DATA1  - orange
+  2:  5V VCC - green
+  3:  GND    - white
+  4:  SENSE  - blue
+  5:  DATA5  - red
+  6:  GND    - grey
+
+  Connect both GNDs and SENSE together.
+*/
